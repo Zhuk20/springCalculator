1da5ec7aa82e27aa3a0aa283c70bd0cc0aab8453
Zhuk20 <ghuk200067@gmail.com>
2023-07-08 23:05:43 +1200

    commit

--- src/main/java/com/spring/calculator/CalculatorService.java
+++
@@ -1,8 +0,0 @@
-package com.spring.calculator;
-
-public interface CalculatorService {
-    int plus(int a, int b);
-    int minus(int a, int b);
-    int multiply(int a, int b);
-    int divide(int a, int b);
-}
